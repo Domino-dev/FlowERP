28fe8e597edf1f7aa675dd5e691919a2c0ee886e
Domino-dev <stepanek.dominik@email.cz>
2025-12-19 17:51:40 +0100

    [FEAT][Fixtures+Config][CompanyUserFixture+doctrine]: Add fixtures

---
+++ app/Fixtures/CompanyUserFixture.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+namespace App\Fixtures;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+use App\Database\CompanyUser;
+
+class CompanyUserFixture implements FixtureInterface {
+
+    /**
+     * Load data fixtures with the passed ObjectManager
+     */
+    public function load(ObjectManager $manager): void {
+	// password: tester123
+	$companyUser = new CompanyUser(
+		\App\Helpers\UUIDGenerator::generateInternalID(), 
+		'Admin',
+		NULL,
+		'999999999',
+		'admin@admin.cz',
+		'$2y$10$yfaDtt/tUgXo.CsfO9zNMOiMVhzKyX6k/Ac8gEgUNYCXYXi9YU8p2',
+		'admin',
+		true);
+	$manager->persist($companyUser);
+	$manager->flush();
+    }
+}
