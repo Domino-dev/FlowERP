0d060b79e99f3deb7aab3a0725c14b42953b5a25
Domino-dev <stepanek.dominik@email.cz>
2025-12-28 17:40:03 +0100

    [FEAT][Presentation][Home]: Add default settings

--- app/Presentation/Home/HomePresenter.php
+++ app/Presentation/Home/HomePresenter.php
@@ -1,12 +1,9 @@
 <?php
-
 declare(strict_types=1);
-
 namespace App\Presentation\Home;
 
 use Nette;
 
-
-final class HomePresenter extends Nette\Application\UI\Presenter
-{
+final class HomePresenter extends \App\Presentation\BasePresenter{
+    
 }
